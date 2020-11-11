8b71c74e75fb7765c5bae39ea9137e03585e1ea7
Tobi Ajibade <herityjohnny14>
2020-11-11 14:16:24 -0800

    Feature for delecting all articles by author

--- routes/articles.js
+++ routes/articles.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { get_all_articles, get_one_article, delete_one_article, create_article, update_one_article, get_author_articles} = require('../controllers/articles');
+const { get_all_articles, get_one_article, delete_one_article, create_article, update_one_article, get_author_articles, delete_all_articles} = require('../controllers/articles');
 const { verify } = require('../middlewares/verify_auth');
 
 router.get('/', get_all_articles);
 router.get('/:authorid', get_author_articles);
 router.get('/:articleid', get_one_article);
 router.delete('/delete/:articleid',verify, delete_one_article);
+router.delete('/delete',verify, delete_all_articles);
 router.patch('/update/:articleid',verify, update_one_article);
 router.post("/create", verify, create_article);
 
